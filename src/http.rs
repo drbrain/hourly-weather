ffa1f7a380011be2f3ba6b0c5fc6e5ee1311ad62
Eric Hodel <drbrain@segment7.net>
2023-02-26 23:24:16 -0800

    Extract http.rs (to allow tracing)

---
+++ src/http.rs
@@ -0,0 +1,20 @@
+use tracing::info;
+
+use crate::{root::root, Args, HourlyWeather};
+use std::{io::Error, net::SocketAddr};
+
+pub async fn start(args: Args) -> Result<(), Error> {
+    let state = HourlyWeather::new("weather.segment7.net");
+    let app = root(&args).with_state(state);
+
+    let tls_config = args.tls_config().await;
+
+    let port = 8443;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+
+    info!("binding {addr:?}");
+
+    axum_server::bind_rustls(addr, tls_config)
+        .serve(app.into_make_service())
+        .await
+}
--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,21 @@ mod activity_pub;
 mod args;
 mod hourly;
 mod hourly_weather;
+mod http;
 mod root;
 mod tracing;
 mod webfinger;
 
 pub use crate::args::Args;
-use crate::root::root;
 pub use crate::webfinger::webfinger;
 use clap::Parser;
 pub use hourly_weather::HourlyWeather;
-use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     tracing::init("hourly-weather");
 
-    let args = Args::parse();
-
-    let state = HourlyWeather::new("weather.segment7.net");
-    let app = root(&args).with_state(state);
-
-    let tls_config = args.tls_config().await;
-
-    let port = 8443;
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
-
-    axum_server::bind_rustls(addr, tls_config)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    http::start(Args::parse()).await.unwrap();
 
     opentelemetry::global::shutdown_tracer_provider();
 
