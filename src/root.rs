b25895019720d0bd979011b6f6744121f457aac2
Eric Hodel <drbrain@segment7.net>
2023-02-26 23:29:58 -0800

    Add healthcheck

--- src/root.rs
+++ src/root.rs
@@ -4,12 +4,15 @@ use axum::{body::HttpBody, routing::get, Router};
 use std::sync::Arc;
 use tower_http::services::{ServeDir, ServeFile};
 
+async fn healthcheck() {}
+
 pub fn root<B>(args: &Args) -> Router<Arc<HourlyWeather>, B>
 where
     B: HttpBody + Send + 'static,
 {
     Router::new()
-        .route("/.well-known/webfinger", get(webfinger))
+        .route("/.well-known/webfinger", get(webfinger).head(webfinger))
+        .route("/healthcheck", get(healthcheck).head(healthcheck))
         .nest("/hourly", hourly::app())
         .nest_service("/images", ServeDir::new(args.images_dir()))
         .route_service("/sky.jpeg", ServeFile::new(args.sky_jpeg()))
