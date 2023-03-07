1f26143e1f0ec0e389675aef3e9153f20dae8b43
Eric Hodel <drbrain@segment7.net>
2023-03-06 22:49:29 -0800

    Try setting up tracing like davidB/sandbox_axum_observability

--- src/root.rs
+++ src/root.rs
@@ -1,21 +1,19 @@
 use crate::{host_meta, hourly, webfinger, Args, HourlyWeather};
-use axum::{body::HttpBody, routing::get, Router};
-use axum_tracing_opentelemetry::opentelemetry_tracing_layer;
+use axum::{routing::get, Router};
+use axum_tracing_opentelemetry::{opentelemetry_tracing_layer, response_with_trace_layer};
 use std::sync::Arc;
 use tower_http::services::{ServeDir, ServeFile};
 
 async fn healthcheck() {}
 
-pub fn root<B>(args: &Args) -> Router<Arc<HourlyWeather>, B>
-where
-    B: HttpBody + Send + 'static,
-{
+pub fn root(args: &Args) -> Router<Arc<HourlyWeather>, hyper::Body> {
     Router::new()
         .route("/.well-known/host-meta", get(host_meta).head(host_meta))
         .route("/.well-known/webfinger", get(webfinger).head(webfinger))
         .nest("/hourly", hourly::app())
         .nest_service("/images", ServeDir::new(args.images_dir()))
         .route_service("/sky.jpeg", ServeFile::new(args.sky_jpeg()))
+        .layer(response_with_trace_layer())
         .layer(opentelemetry_tracing_layer())
         .route("/healthcheck", get(healthcheck).head(healthcheck))
 }
--- src/tracing.rs
+++ src/tracing.rs
@@ -1,4 +1,33 @@
+use axum_tracing_opentelemetry::{jaeger, resource::DetectResource};
+use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt};
+
 pub fn init() {
-    axum_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
-        .expect("Failed to initialize tracing");
+    std::env::set_var(
+        "RUST_LOG",
+        std::env::var("RUST_LOG")
+            .or_else(|_| std::env::var("OTEL_LOG_LEVEL"))
+            .unwrap_or("hourly_weather=debug".to_string()),
+    );
+
+    let otel_resource = DetectResource::default()
+        .with_fallback_service_name("hourly_weather")
+        .with_fallback_service_version(env!("CARGO_PKG_VERSION"))
+        .build();
+
+    let otel_tracer = jaeger::init_tracer(otel_resource, jaeger::identity)
+        .expect("failed to initialize OTEL tracer");
+
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(otel_tracer);
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .json()
+        .with_timer(tracing_subscriber::fmt::time::uptime());
+
+    let subscriber = tracing_subscriber::registry()
+        .with(fmt_layer)
+        .with(EnvFilter::from_default_env())
+        .with(otel_layer);
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("failed to register global tracing subscriber");
 }
